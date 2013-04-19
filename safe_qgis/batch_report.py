c42ac8e21ddde37f9e39776f92354a6b541695cf
Gigih Aji Ibrahim <bungcip@gmail.com>
2013-04-19 07:46:57 +0700

    batch report

--- safe_qgis/batch_report.py
+++ safe_qgis/batch_report.py
@@ -1,1 +1,45 @@
-__author__ = 'bungcip'
+"""
+InaSAFE Disaster risk assessment tool developed by AusAid -
+**Script runner dialog.**
+
+Contact : ole.moller.nielsen@gmail.com
+
+.. note:: This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 2 of the License, or
+     (at your option) any later version.
+
+"""
+
+__author__ = 'bungcip@gmail.com & tim@linfiniti.com'
+__revision__ = '$Format:%H$'
+__date__ = '01/10/2012'
+__copyright__ = ('Copyright 2012, Australia Indonesia Facility for '
+                 'Disaster Reduction')
+
+import sys
+
+from PyQt4.QtGui import QDialog, QApplication
+from safe_qgis.batch_report_base import Ui_BatchReportBase
+
+
+class BatchReport(QDialog, Ui_BatchReportBase):
+    def __init__(self, theParent=None):
+        """Constructor for the dialog.
+
+        Args:
+           theParent - Optional widget to use as parent.
+        Returns:
+           not applicable
+        Raises:
+           no exceptions explicitly raised
+        """
+        QDialog.__init__(self, theParent)
+        self.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = BatchReport(None)
+    form.show()
+    app.exec_()
